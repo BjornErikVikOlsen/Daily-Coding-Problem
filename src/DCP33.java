45e0ca643571223b45aea0a54f260b0af82b0428
BjornErikVikOlsen <94bevo@gmail.com>
2019-08-25 14:20:55 +0200

    Added new methods. Code fix

--- src/DCP33.java
+++ src/DCP33.java
@@ -6,29 +6,44 @@ public class DCP33 {
 
     public static void main(String[] args) {
 
-        List arr = new ArrayList();
+        List <Double> arr = new ArrayList<>();
+        arr.add(1.0);
+        arr.add(5.0);
         DCP33 test = new DCP33();
         test.GetNumber(arr);
 
-
-    }
-    public void Median(List arr){
-
-        int median = 0;
-        //for(int numb : arr)
-
-        System.out.println("Is this your number? " +);
-
-
-
     }
 
-    public void GetNumber(List arr){
-        Scanner findInt = new Scanner(System.in);
+
+    public void GetNumber(List<Double> arr){
+        Scanner findNumber = new Scanner(System.in);
         System.out.println("Enter number");
-        int k = findInt.nextInt();
-        arr.add(k);
-        Median(arr);
+        //int k = findNumber.nextInt();
+        arr.add(findNumber.nextDouble());
+        FindSum(arr);
+    }
+
+    public double FindSum(List<Double> arr){
+
+        double median;
+        double sum = 0.0;
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr.get(i);
+
+        }
+        Median(arr, sum);
+        return sum;
+    }
+
+    public void Median(List <Double> arr, Double sum){
+        int ListSize = arr.size();
+        double DListSize = ListSize;
+        double median = 0.0;
+        median = sum/ListSize;
+
+        System.out.println(median);
+        GetNumber(arr);
+
     }
 
 

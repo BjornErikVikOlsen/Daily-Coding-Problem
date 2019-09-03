355a46c90ca11ee0e2b8b8d72904fd12fbbe7d08
BjornErikVikOlsen <94bevo@gmail.com>
2019-09-03 20:56:15 +0200

    Modified DCP0

--- src/DCP0.java
+++ src/DCP0.java
@@ -4,4 +4,20 @@ Implement FizzBuzz
 
 public class DCP0 {
 
+    public void FizzBuzz(){
+        for(int i = 0; i < 100; i++){
+            String a = "";
+
+            
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DCP0 test = new DCP0();
+        test.FizzBuzz();
+
+    }
+
 }

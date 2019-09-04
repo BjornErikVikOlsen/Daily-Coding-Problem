dfd9f53a2c36fa94c1fa382035cc159ee88e5dd4
BjornErikVikOlsen <94bevo@gmail.com>
2019-09-04 22:39:31 +0200

    Modified DCP0

--- src/DCP0.java
+++ src/DCP0.java
@@ -5,12 +5,20 @@ Implement FizzBuzz
 public class DCP0 {
 
     public void FizzBuzz(){
-        for(int i = 0; i < 100; i++){
+        for(int i = 1; i < 100; i++){
             String a = "";
-
-            
+            if (i%3 == 0){
+                a = "Fizz";
+            }
+            else if (i%5 == 0){
+                a = "Buzz";
+            }
+            else
+                a = Integer.toString(i);
+                System.out.println(a);
 
         }
+
     }
 
     public static void main(String[] args) {

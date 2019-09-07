ea1ca27e15f8cd5775906492d5d9498fe29a6d17
BjornErikVikOlsen <94bevo@gmail.com>
2019-09-07 22:16:00 +0200

    Modified DCP0

--- src/DCP0.java
+++ src/DCP0.java
@@ -21,10 +21,30 @@ public class DCP0 {
 
     }
 
+    public void FizzBuzz2(){
+        for(int i = 1; i<100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else
+                System.out.println(i);
+        }
+    }
+
+    public void FizzBuzz3(){
+        
+    }
+
     public static void main(String[] args) {
 
         DCP0 test = new DCP0();
-        test.FizzBuzz();
+        test.FizzBuzz2();
 
     }
 

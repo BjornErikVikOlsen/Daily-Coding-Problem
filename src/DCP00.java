1911b53244a50be5dc75a2374b7e77f69ed65134
BjornErikVikOlsen <94bevo@gmail.com>
2019-09-18 23:05:10 +0200

    Modified DCP100

--- src/DCP00.java
+++ src/DCP00.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class DCP00 {
 
     public static void main(String[] args) {
-        List arr = new ArrayList<Integer>();
+        ArrayList arr = new ArrayList<Integer>();
         arr.add(2);
         arr.add(4);
         arr.add(6);
@@ -12,7 +12,8 @@ public class DCP00 {
         int [] arra = {2,4,6};
 
         DCP00 test = new DCP00();
-        test.PrintNum(arra, 5);
+        //test.PrintNum(arra, 5);
+        test.PrintNum2(arr, 5);
     }
 
     public void PrintNum(int [] arra, int k){
@@ -25,9 +26,13 @@ public class DCP00 {
 
     }
 
-    public void PrintNum2(ArrayList arr){
+    public void PrintNum2(ArrayList<Integer> arr, int k){
         for(int i = 0; i < arr.size(); i++){
-            
+
+            int l = arr.get(i);
+            if(l < k){
+                System.out.println(l);
+            }
         }
     }
 }

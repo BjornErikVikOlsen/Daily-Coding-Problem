017c17903165a4d2b67f68fa163a9c8a8c398654
BjornErikVikOlsen <94bevo@gmail.com>
2019-09-26 21:44:53 +0200

    Added DCP118

---
+++ src/DCP118.java
@@ -0,0 +1,22 @@
+/*
+Given a sorted list of integers, square the elements and give the output in sorted order.
+
+For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
+
+ */
+
+import java.util.ArrayList;
+
+public class DCP118 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        int[] ar = {-9, -2, 0, 2, 3};
+
+    }
+
+    public void SquareElem(){
+
+    }
+
+
+}

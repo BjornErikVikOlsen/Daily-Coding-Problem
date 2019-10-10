1f7c4612745f91fe924589a65ae445064082103f
BjornErikVikOlsen <94bevo@gmail.com>
2019-10-10 22:07:44 +0200

    Added DCP132

---
+++ src/DCP132.java
@@ -0,0 +1,30 @@
+/*
+Design and implement a HitCounter class that keeps track of requests (or hits). It should support the following operations:
+
+    record(timestamp): records a hit that happened at timestamp
+    total(): returns the total number of hits recorded
+    range(lower, upper): returns the number of hits that occurred between timestamps lower and upper (inclusive)
+
+Follow-up: What if our system has limited memory?
+ */
+
+import java.sql.Timestamp;
+
+public class DCP132 {
+
+    public void recordTime(){
+
+    }
+
+    public void totalTime(){
+
+    }
+
+    public void rangeTime(){
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

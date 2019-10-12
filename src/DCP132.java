b1f9b84e82b6638d36bf55dc7aabb9660b82b08c
BjornErikVikOlsen <94bevo@gmail.com>
2019-10-12 23:48:29 +0200

    modified DCP132

--- src/DCP132.java
+++ src/DCP132.java
@@ -9,22 +9,30 @@ Follow-up: What if our system has limited memory?
  */
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class DCP132 {
 
-    public void recordTime(){
+    ArrayList arr = new ArrayList();
 
+    public void recordTime(){
+        Date date = new Date();
+        arr.add(date);
     }
 
     public void totalTime(){
-
+        System.out.println(arr.size());
     }
 
-    public void rangeTime(){
+    public void rangeTime(int upper, int lower){
         
     }
 
     public static void main(String[] args) {
-
+        DCP132 test = new DCP132();
+        test.recordTime();
+        test.recordTime();
+        test.totalTime();
     }
 }

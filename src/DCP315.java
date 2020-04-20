ddaf9e7bf9cb68fd82b662153cfadf54bf8036b5
BjornErikVikOlsen <94bevo@gmail.com>
2020-04-20 22:09:54 +0200

    Added DCP324 and 325

--- src/DCP315.java
+++ src/DCP315.java
@@ -13,4 +13,14 @@ Here is an example:
 Write a program to determine whether a given input is a Toeplitz matrix.
  */
 public class DCP315 {
+
+    public static void main(String[] args) {
+        int [][] matrix = {{1,2,3,4,8},{5,1,2,3,4},{4,5,1,2,4},{7,4,5,1,2}};
+
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix.length; j++){
+
+            }
+        }
+    }
 }

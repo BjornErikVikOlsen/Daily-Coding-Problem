aff885c059d517f2b7d656fcc6585bc5e193a59f
BjornErikVikOlsen <94bevo@gmail.com>
2021-01-29 23:45:14 +0100

    Modified DCP

--- src/DCP0.java
+++ src/DCP0.java
@@ -39,7 +39,15 @@ public class DCP0 {
 
     public void FizzBuzz3(){
         for(int i = 1; i<100; i++){
-
+            if (i % 3 == 0){
+                System.out.print("Fizz");
+            }
+            else if (i % 5 == 0){
+                System.out.print("Buzz");
+            }
+            else {
+                System.out.print(i);
+            }
         }
     }
 

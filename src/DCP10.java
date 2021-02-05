4ba4646e32ca0576080a4e3fd556ea625af79014
BjornErikVikOlsen <94bevo@gmail.com>
2021-02-05 23:52:52 +0100

    Modified DCP

--- src/DCP10.java
+++ src/DCP10.java
@@ -1,9 +1,39 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 /*
 Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
  */
 public class DCP10 {
 
     public static void main(String[] args) {
+        solution(new Command() {
+            @Override
+            public void execute() {
+                {
+                    System.out.println("Hello World");
+                }
+            }
+        }, 1000);
+
+        // java 8
+        solution(() -> System.out.println("Hello World"), 1000);
+    }
+
+    public static void solution(Command command, int delay) {
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                command.execute();
+            }
+        }, delay);
 
     }
+
+    interface Command {
+        public void execute();
+    }
+
 }
+

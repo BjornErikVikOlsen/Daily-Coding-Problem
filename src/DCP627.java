db650ba1145767f6c91da8fd3bd62a6962462450
Bjørn Erik Vik Olsen <94bevo@gmail.com>
2021-02-21 22:52:10 +0100

    added simple DCP

---
+++ src/DCP627.java
@@ -0,0 +1,32 @@
+/*
+Given an iterator with methods next() and hasNext(), create a wrapper iterator, PeekableInterface, which also implements peek(). peek shows the next element that would be returned on next().
+
+Here is the interface:
+
+class PeekableInterface(object):
+    def __init__(self, iterator):
+        pass
+
+    def peek(self):
+        pass
+
+    def next(self):
+        pass
+
+    def hasNext(self):
+        pass
+
+ */
+public class DCP627 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class PeekableInterface {
+
+        public int peek(){
+            return 0;
+        }
+    }
+}
